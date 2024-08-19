fe6cebeb1694081192b782727a67acb835e13c50
sunny.park <seolseee@gmail.com>
2024-08-19 15:50:39 +0900

    [setting] : 프로젝트 초기 세팅

---
+++ src/test/java/com/threeoh/HowAbout/HowAboutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threeoh.HowAbout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowAboutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
